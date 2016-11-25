f524c96e5afc03040b0152e8b750c9aa5bcf75a8
byunghyunpark <openmind8735@gmail.com>
2016-11-25 15:57:23 +0900

    AWS RDS 연결 세팅
    - ebignore 추가
    - settings.py에 DEBUG 모드 예외처리
    - ebextensions에 psycopg2관련 yum 설정추가
    - settings에 static root 설정
    - 배포시 자동 실행 command 추가( collectstatic, migrate, createsuperuser )
    - member app 추가
    - psycopg2 패키지 requirements.txt 추가

---
+++ django_app/member/management/commands/createsu.py
@@ -0,0 +1,23 @@
+import json
+import os
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.management import BaseCommand
+
+User = get_user_model()
+CONF_DIR = settings.CONF_DIR
+config = json.loads(open(os.path.join(CONF_DIR, 'settings_deploy.json')).read())
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        email = config['defaultSuperuser']['email']
+        password = config['defaultSuperuser']['password']
+        if not User.objects.filter(email=email).exist():
+            User.objects.create_superuser(
+                email=email,
+                password=password
+            )
+        else:
+            print('default superuser exist')
--- django_app/mysite/settings.py
+++ django_app/mysite/settings.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import sys
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+CONF_DIR = os.path.join(ROOT_DIR, '.django-conf')
+STATIC_ROOT = os.path.join(ROOT_DIR, 'static_root')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -23,7 +28,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zfl#cqk^ktsz%^*y3ekq0r3vx4&&p1!!i$j%i!=pscy79lqtf#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = (len(sys.argv) > 1 and sys.argv[1] == 'runserver')
+
+if DEBUG:
+    config = json.loads(open(os.path.join(CONF_DIR, 'settings_debug.json')).read())
+else:
+    config = json.loads(open(os.path.join(CONF_DIR, 'settings_deploy.json')).read())
 
 ALLOWED_HOSTS = [
     'popcorn-backend-dev.ap-northeast-2.elasticbeanstalk.com',
@@ -31,7 +42,6 @@ ALLOWED_HOSTS = [
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -73,19 +83,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = config['databases']
 
 
 # Password validation
-# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -103,20 +112,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.10/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
 STATIC_URL = '/static/'
