383da3abb2b588e21c6ed595454a42d64ef959c1
byunghyunpark <openmind8735@gmail.com>
2016-12-01 17:50:21 +0900

    Bugfix

--- django_app/movie/serializers/comment.py
+++ django_app/movie/serializers/comment.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from member.serializers import UserSerializer
 from movie.models import Comment
+from movie.serializers.movie import MovieSerializer
 
 
 class CommentSerializer(serializers.ModelSerializer):
--- django_app/movie/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from movie.apis.comment import CommentAPIView, CommentDetailAPIView
-
-
-urlpatterns = [
-    url(r'^comment/$', CommentAPIView.as_view()),
-    url(r'^comment/(?P<comment_id>[0-9]+)/$', CommentDetailAPIView.as_view()),
-]
