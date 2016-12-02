a949c8e940f856a7fc53e79c7f2b52ace8428c54
Albert Han <alberthan92@gmail.com>
2016-12-02 19:46:10 +0900

    영화 URLS 추가
    *영화 상세 URLS 추가

--- django_app/movie/urls/apis.py
+++ django_app/movie/urls/apis.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
 from movie.apis.comment import CommentAPIView, CommentDetailAPIView
-from movie.apis.movie_search import MovieSearch
+from movie.apis.movie_search import MovieSearch, MovieDetail
 
 urlpatterns = [
-    url(r'', MovieSearch.as_view(), name='movie_search'),
+    url(r'^search/$', MovieSearch.as_view(), name='movie_search'),
+    url(r'^(?P<pk>[0-9]+)/$', MovieDetail.as_view(), name='database_movie'),
     url(r'^comment/$', CommentAPIView.as_view()),
     url(r'^comment/(?P<comment_id>[0-9]+)/$', CommentDetailAPIView.as_view()),
 ]
