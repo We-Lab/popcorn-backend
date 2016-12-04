9bf73772b185cc5a9258d25d594f7a6df7b1f398
Albert Han <alberthan92@gmail.com>
2016-12-05 01:19:53 +0900

    영화 검색 기능 조건 추가
    *영화 제목에 띄어쓰기 고려
    *띄어쓰기 있으면 리스트화 시킨다
    *첫 번째 단어만 받아서 데이터베이스에 있는 KOR/ENG 제목을 다 검색한다

--- django_app/movie/apis/movie_search.py
+++ django_app/movie/apis/movie_search.py
@@ -1,6 +1,4 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.exceptions import APIException, NotAcceptable
+from rest_framework.exceptions import NotAcceptable
 from rest_framework.views import APIView
 from movie.models import Movie
 from rest_framework.response import Response
@@ -11,16 +9,30 @@ from movie.serializers.movie import MovieDetailSerializer, MovieSerializer
 class MovieSearch(APIView):
     def get(self, request, pk=None):
         keyword = request.GET.get('keyword')
-        # keyword = "".join(keyword.split())
-        daum_id = request.GET.get('daum_id', '')
-        movie = movie_search(keyword)
+        keyword = "".join(keyword.split())
+        title = movie_search(keyword)
+        hash_kor = title[0].split()[0]
+        hash_eng = title[1].split()[0]
 
-        if keyword:
+        if Movie.objects.filter(title_kor__icontains=keyword):
             movie = Movie.objects.filter(title_kor__icontains=keyword)
+            serializer = MovieSerializer(movie, many=True)
+            return Response(serializer.data)
+        elif Movie.objects.filter(title_eng__icontains=keyword):
+            movie = Movie.objects.filter(title_eng__icontains=keyword)
+            serializer = MovieSerializer(movie, many=True)
+            return Response(serializer.data)
+        elif Movie.objects.filter(title_kor__icontains=hash_kor):
+            movie = Movie.objects.filter(title_kor__icontains=hash_kor)
+            serializer = MovieSerializer(movie, many=True)
+            return Response(serializer.data)
+        elif Movie.objects.filter(title_eng__icontains=hash_eng):
+            movie = Movie.objects.filter(title_eng__icontains=hash_eng)
             serializer = MovieSerializer(movie, many=True)
             return Response(serializer.data)
         else:
             raise NotAcceptable('keyword is required')
+
 
 class MovieDetail(APIView):
     def get(self, request, pk):
