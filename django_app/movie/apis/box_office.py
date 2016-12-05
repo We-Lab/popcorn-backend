138dfe1ee0b53501a5a13749a94efe638842a635
byunghyunpark <openmind8735@gmail.com>
2016-12-05 21:48:27 +0900

    Box office 크롤링 개수 변경 (20개 -> 10개), rank field 추가

--- django_app/movie/apis/box_office.py
+++ django_app/movie/apis/box_office.py
@@ -9,7 +9,7 @@ class BoxOfficeAPIView(APIView):
 
     # 최근에 업데이트된 탑 20위 박스오피스 노출함
     def get(self, request, *args, **kwargs):
-        box_office_reversed = BoxOfficeMovie.objects.all().order_by('-created_date')[:20]
+        box_office_reversed = BoxOfficeMovie.objects.all().order_by('-created_date')[:10]
         box_office = reversed(box_office_reversed)
         serializer = BoxOfficeSerialize(box_office, many=True)
         return Response(serializer.data)
