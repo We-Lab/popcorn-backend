51ec9b5fca41c9818a9dd2d08949e6d981078a26
Albert Han <alberthan92@gmail.com>
2016-12-11 00:51:41 +0900

    매거진 검색 기능
    *매거진 텍스트 저장 방법 수정
     1. 텍스트 하나로 합치기

--- django_app/apis/magazine_search.py
+++ django_app/apis/magazine_search.py
@@ -9,13 +9,13 @@ def magazine_search():
 
     for actor in actors:
         actor_id = actor.daum_id
+        print('actor: {}'.format(actor_id))
         response = requests.get("http://movie.daum.net/person/main?personId={}".format(actor_id))
         bs_response = BeautifulSoup(response.text, "html.parser")
         try:
             link = bs_response.select('a.link_magazine')[0]['href']
             img_link = bs_response.select('a.link_magazine')[0].select('span.magazine_img')
             img_url = re.findall(r'\((.*)\)', img_link[0]['style'])
-            print(img_url)
             mag_id = re.findall(r'\d+', link)[1]
             print(mag_id)
             if Magazine.objects.filter(mag_id=mag_id).exists():
@@ -25,18 +25,20 @@ def magazine_search():
                 mag = requests.get(link)
                 bs_mag = BeautifulSoup(mag.text, 'html.parser')
                 mag_title = bs_mag.select('h3.tit_view')[0].text
-                body = bs_mag.select('div.section_view.section_editor p')
+                body = bs_mag.select('div.section_view.section_editor')
                 for count in range(len(body)):
-                    body = bs_mag.select('div.section_view.section_editor p')[count].text
-                    text.append(body)
-                if len(text) < 20:
-                    pass
-                else:
+                    main_body = body.select('div.section_view.section_editor p')[count].text
+                    text.append(main_body)
+                mag_text = "\n".join(text)
+                if len(text) > 20:
                     Magazine.objects.get_or_create(
                         mag_id=mag_id,
                         img_url=img_url[0],
                         title=mag_title,
-                        content=text,
+                        content=mag_text,
                     )
+                else:
+                    pass
+
         except:
             pass
