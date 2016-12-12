d8702edca695b7824f29f9d066c573bc90c07823
byunghyunpark <openmind8735@gmail.com>
2016-12-12 14:32:33 +0900

    local db postgres 로 변경

--- django_app/mysite/settings.py
+++ django_app/mysite/settings.py
@@ -200,12 +200,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+    DATABASES = config['databases']
+    #
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     }
+    # }
 else:
     DATABASES = config['databases']
 
