83a1dd55e43bcfae560f8e9ce14a38b75163d863
byunghyunpark <openmind8735@gmail.com>
2016-12-17 16:42:08 +0900

    별점 최상위 20개 영화 기능추가

---
+++ django_app/movie/apis/movie_rank.py
@@ -0,0 +1,12 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from movie.models import Movie
+from movie.serializers.movie import MovieDetailSerializer
+
+
+class StarRankView(APIView):
+    def get(self, request, *args, **kwargs):
+        movie = Movie.objects.all().order_by('-star_average')[:20]
+        serializer = MovieDetailSerializer(movie, many=True)
+        return Response(serializer.data)
