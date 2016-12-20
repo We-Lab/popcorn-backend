a8310b2c3921a576c7b4cd0600b41b8735b716d2
byunghyunpark <openmind8735@gmail.com>
2016-12-20 23:00:11 +0900

    유저 취향 초기화 기능 추가

--- django_app/movie/apis/favorites.py
+++ django_app/movie/apis/favorites.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from member.models import MyUser
 from movie.models import Genre, Grade, MakingCountry
@@ -35,3 +37,22 @@ class UserFavorites(generics.ListAPIView):
     def get_queryset(self):
         user_pk = self.request.user.pk
         return MyUser.objects.filter(pk=user_pk)
+
+
+class DeleteFavorite(APIView):
+    """
+    회원의 취향을 초기화합니다.
+    """
+    def post(self, request, *args, **kwargs):
+        user = MyUser.objects.get(id=self.request.user.pk)
+
+        genre = user.favorite_genre.all()
+        user.favorite_genre.remove(*genre)
+
+        grade = user.favorite_grade.all()
+        user.favorite_grade.remove(*grade)
+
+        making_country = user.favorite_making_country.all()
+        user.favorite_making_country.remove(*making_country)
+
+        return Response({"detail": "유저 취향을 초기화했습니다"})
