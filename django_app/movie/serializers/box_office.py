ab4530f97e34206737c2d6aeb7850d789d122a18
byunghyunpark <openmind8735@gmail.com>
2016-12-20 20:52:12 +0900

    ios 박스오피스에 is comment, is like 추가

--- django_app/movie/serializers/box_office.py
+++ django_app/movie/serializers/box_office.py
@@ -30,3 +30,17 @@ class BoxOfficeSerializerIOS(serializers.ModelSerializer):
             'release_date',
             'ticketing_rate',
         )
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['is_like'] = False
+        ret['is_comment'] = False
+        request = self.context.get('request')
+        if request is not None:
+            if request.user.is_authenticated:
+                if instance.like_users.filter(id=request.user.pk).exists():
+                    ret['is_like'] = True
+                if instance.comment_users.filter(id=request.user.pk).exists():
+                    ret['is_comment'] = True
+        return ret
+
