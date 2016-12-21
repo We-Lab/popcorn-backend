e26190abbc917768b665ec90c32f693ae3d98e3a
byunghyunpark <openmind8735@gmail.com>
2016-12-22 00:22:18 +0900

    유저취향 view 주석추가

--- django_app/movie/apis/favorites.py
+++ django_app/movie/apis/favorites.py
@@ -1,3 +1,8 @@
+""" 취향 module
+1. 유저의 취향 항목을 각각 리스트로 출력합니다. => 메인 페이지와 회원정보 페이지에 사용됩니다.
+2. 유저의 취향 리스트도 출력합니다.
+3. 취향은 초기화 가능합니다.
+"""
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework.response import Response
@@ -10,6 +15,9 @@ from movie.serializers.movie import GenreSerializer, MakingCountrySerializer, Gr
 
 
 class GenreView(generics.ListAPIView):
+    """
+    장르 리스트 출력
+    """
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     permission_classes = (permissions.IsAuthenticated,)
@@ -17,6 +25,9 @@ class GenreView(generics.ListAPIView):
 
 
 class GradeView(generics.ListAPIView):
+    """
+    관람등릅 리스트 출력
+    """
     queryset = Grade.objects.all()
     serializer_class = GradeSerializer
     permission_classes = (permissions.IsAuthenticated,)
@@ -24,6 +35,9 @@ class GradeView(generics.ListAPIView):
 
 
 class MakingCountryView(generics.ListAPIView):
+    """
+    제작국가 리스트 출력
+    """
     queryset = MakingCountry.objects.all()
     serializer_class = MakingCountrySerializer
     permission_classes = (permissions.IsAuthenticated,)
@@ -31,6 +45,9 @@ class MakingCountryView(generics.ListAPIView):
 
 
 class UserFavorites(generics.ListAPIView):
+    """
+    유저의 취향 리스트 출력
+    """
     serializer_class = UserFavoritesSerializer
     paginator = None
 
@@ -41,17 +58,20 @@ class UserFavorites(generics.ListAPIView):
 
 class DeleteFavorite(APIView):
     """
-    회원의 취향을 초기화합니다.
+    유저 취향 초기화
     """
     def post(self, request, *args, **kwargs):
         user = MyUser.objects.get(id=self.request.user.pk)
 
+        # 장르 초기화
         genre = user.favorite_genre.all()
         user.favorite_genre.remove(*genre)
 
+        # 관람등급 초기화
         grade = user.favorite_grade.all()
         user.favorite_grade.remove(*grade)
 
+        # 제작국가 초기화
         making_country = user.favorite_making_country.all()
         user.favorite_making_country.remove(*making_country)
 
