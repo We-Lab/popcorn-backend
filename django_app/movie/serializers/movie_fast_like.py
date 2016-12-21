c0900f3a9e9376ede489fae3d10ff879070a90c0
byunghyunpark <openmind8735@gmail.com>
2016-12-22 03:29:56 +0900

    영화 빠른 좋아요 시리얼라이저 주석추가

--- django_app/movie/serializers/movie_fast_like.py
+++ django_app/movie/serializers/movie_fast_like.py
@@ -4,7 +4,9 @@ from movie.models import Movie
 
 
 class MovieFastLikeSerializer(serializers.ModelSerializer):
-
+    """
+    빠른 영화평가 직렬화
+    """
     class Meta:
         model = Movie
         fields = (
@@ -14,6 +16,7 @@ class MovieFastLikeSerializer(serializers.ModelSerializer):
             'created_year',
         )
 
+    # 영화 좋아요 작성여부, 댓글 작성여부 추가
     def to_representation(self, instance):
         ret = super().to_representation(instance)
         ret['is_like'] = False
