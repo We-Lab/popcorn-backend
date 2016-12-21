2cc947d237b4c24a62edfe3aff34c1c52895b4ea
byunghyunpark <openmind8735@gmail.com>
2016-12-22 03:28:44 +0900

    취향 시리얼라이저 주석추가

--- django_app/movie/serializers/favorites.py
+++ django_app/movie/serializers/favorites.py
@@ -4,6 +4,9 @@ from member.models import MyUser
 
 
 class UserFavoritesSerializer(serializers.ModelSerializer):
+    """
+    유저 취향정보 직렬화
+    """
     class Meta:
         model = MyUser
         fields = (
