08d36c135b8ffef2926ee5f099f6474899aa4648
byunghyunpark <openmind8735@gmail.com>
2016-12-22 00:22:58 +0900

    명대사 view 주석추가

--- django_app/movie/apis/magazine.py
+++ django_app/movie/apis/magazine.py
@@ -1,3 +1,7 @@
+""" 매거진 view module
+1. 다음영화에서 크롤링한 매거진을 출력합니다.
+2. 메인페이지에 4개의 매거진을 랜덤 출력합니다.
+"""
 import random
 
 from rest_framework.exceptions import NotAcceptable
@@ -8,6 +12,9 @@ from movie.models import Magazine
 
 
 class MagazineList(APIView):
+    """
+    매거진 전체 리스트
+    """
     def get(self, request):
         try:
             magazine = Magazine.objects.all()
@@ -18,6 +25,9 @@ class MagazineList(APIView):
 
 
 class SampleMagazineAPIView(APIView):
+    """
+    매거진 4개 랜덤 출력
+    """
     def get(self, request, *args, **kwargs):
         try:
             magazines = Magazine.objects.all()
@@ -26,5 +36,3 @@ class SampleMagazineAPIView(APIView):
             return Response(serializer.data)
         except:
             raise NotAcceptable('not reachable')
-
-
