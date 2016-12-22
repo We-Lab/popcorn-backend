2786d0060ce0d22a5f14b853bd785a898284cff7
byunghyunpark <openmind8735@gmail.com>
2016-12-22 12:22:44 +0900

    base model 주석추가

--- django_app/mysite/utils/models.py
+++ django_app/mysite/utils/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class BaseModel(models.Model):
+    """
+    data의 작성일과 수정일을 module 화
+    """
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
