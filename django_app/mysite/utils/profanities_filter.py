d4798601a30e56df0afc782f37c2288b73c5f0e2
byunghyunpark <openmind8735@gmail.com>
2016-12-22 13:10:20 +0900

    욕설 필터링 주석추가

--- django_app/mysite/utils/profanities_filter.py
+++ django_app/mysite/utils/profanities_filter.py
@@ -1,3 +1,8 @@
+""" 욕설필터 module
+
+욕설을 특수문자로 변경해서 저장합니다.
+
+"""
 import random
 import re
 
@@ -5,6 +10,10 @@ from mysite.settings import BAD_WORDS
 
 
 class ProfanitiesFilter(object):
+    """
+    1. 욕설 필터링
+    2. 욕설 list는 django-config/bad_words.json 파일에서 관리함
+    """
     def __init__(self, ignore_case=True, replacements='$@%-?!', inside_words=True):
         """
         :param filterlist: 금지 단어
@@ -18,20 +27,36 @@ class ProfanitiesFilter(object):
         self.inside_words = inside_words
 
     def _make_clean_word(self, length):
-        # length 만큼 대체 문자 출력
+        """
+        length 만큼 대체 문자 출력
+
+        :param length: 단어길이
+        :return: 특수문자화 된 단어
+        """
         return ''.join([random.choice(self.replacements) for i in range(length)])
 
     def __replacer(self, match):
+        """
+        일치하는 regex 객체를 받아서 text로 변경하고 길이만큼 랜덤특수문자 출력
+
+        :param match: 일치하는 regex 객체
+        :return: 변경된 텍스트
+        """
         value = match.group()
         return self._make_clean_word(len(value))
 
     def clean(self, text):
+        """
+        1. inside_words, ignore_case 값을 확인하고 regex compile
+        2. 패턴과 일치하는 횟수만큼 __replacer 호출하여 텍스트 변경
+
+        :param text: 입력 텍스트
+        :return: 변환된 텍스트
+        """
         regexp_inside_words = {
             True: r'(%s)',
             False: r'\b(%s)\b',
         }
-        # (a|b)g => ag, bg
         regexp = (regexp_inside_words[self.inside_words] % '|'.join(self.bad_words))
         r = re.compile(regexp, re.IGNORECASE if self.ignore_case else 0)
-        # re.sub 사용법 https://docs.python.org/2/library/re.html#re.sub
         return r.sub(self.__replacer, text)
