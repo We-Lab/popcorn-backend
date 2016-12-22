03a3ab44b7a9f1595abe4aa63eaab9ef5e0ca700
byunghyunpark <openmind8735@gmail.com>
2016-12-22 13:19:48 +0900

    유저 관리자 어드민 주석추가

--- django_app/member/admin.py
+++ django_app/member/admin.py
@@ -4,6 +4,9 @@ from member.models import MyUser
 
 
 class MyUserAdmin(admin.ModelAdmin):
+    """
+    유저정보 관리자 어드민
+    """
     fieldsets = [
         ('기본정보', {'fields': ['username', 'nickname', 'email', 'gender', 'date_of_birth', 'date_joined']}),
         ('취향정보', {'fields': ['favorite_genre', 'favorite_grade', 'favorite_making_country']}),
